5d70d7ec14bab3d1e223cc1eccb273d0cce11dbd
finality5 <tanagon601@gmail.com>
2021-01-06 01:19:49 +0700

    add file :dog:

---
+++ src/core/config.js
@@ -0,0 +1,11 @@
+// Replace with your own firebase config!
+export const FIREBASE_CONFIG = {
+  apiKey: 'AIzaSyAfG1NOIhN7LdB6o-3rz9YmRN-mhPnt_qk',
+  authDomain: 'opgrade.firebaseapp.com',
+  databaseURL: 'https://opgrade-default-rtdb.firebaseio.com',
+  projectId: 'opgrade',
+  storageBucket: 'opgrade.appspot.com',
+  messagingSenderId: '314201452178',
+  appId: '1:314201452178:web:2eba8e89859f5d2dde004b',
+  measurementId: 'G-5JRDYRQ33B',
+}
---
+++ src/screens/StartScreen.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import Background from '../components/Background'
+import Logo from '../components/Logo'
+import Header from '../components/Header'
+import Button from '../components/Button'
+import Paragraph from '../components/Paragraph'
+import HeaderAppName from '../components/HeaderAppName'
+
+const StartScreen = ({ navigation }) => (
+  <Background>
+    <HeaderAppName>Opgrade</HeaderAppName>
+    <Logo />
+    <Header>Camera Grading App</Header>
+    <Paragraph>
+      Start grading your exams in easier way for better quality of measurement.
+    </Paragraph>
+    <Button mode="contained" onPress={() => navigation.navigate('LoginScreen')}>
+      Login
+    </Button>
+    <Button
+      mode="outlined"
+      onPress={() => navigation.navigate('RegisterScreen')}
+    >
+      Sign Up
+    </Button>
+  </Background>
+)
+
+export default StartScreen
